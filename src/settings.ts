183294c75a5af287290c52f150dfce4cb8dbdd77
ielliott75 <155854398+ielliott75@users.noreply.github.com>
2026-01-04 08:39:18 +1100

    Rename platform constant to 'ActronAir4Zone'

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'homebridge-actronair4zone';
+export const PLATFORM_NAME = 'ActronAir4Zone';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
