3f2ff724f62a5d330cc1a6ad736012ff86bf60f1
ielliott75 <155854398+ielliott75@users.noreply.github.com>
2026-01-04 09:49:47 +1100

    Refactor heating/cooling state logic for clarity

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -137,16 +137,16 @@ export class ActronAirAccessory {
           case 'auto':
             {
               const tempDiff = this.state.TargetTemperature - this.state.CurrentTemperature;
+              if (Math.abs(tempDiff) < 0.5) {
+                this.state.CurrentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.OFF;
+              } else if (tempDiff > 0) {
+                this.state.CurrentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.HEAT;
+              } else {
+                this.state.CurrentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.COOL;
+              }
+              this.state.TargetHeatingCoolingState = this.platform.Characteristic.TargetHeatingCoolingState.AUTO;
+              break;
             }
-            if (Math.abs(tempDiff) < 0.5) {
-              this.state.CurrentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.OFF;
-            } else if (tempDiff > 0) {
-              this.state.CurrentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.HEAT;
-            } else {
-              this.state.CurrentHeatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.COOL;
-            }
-            this.state.TargetHeatingCoolingState = this.platform.Characteristic.TargetHeatingCoolingState.AUTO;
-            break;
           }
         }
       }
